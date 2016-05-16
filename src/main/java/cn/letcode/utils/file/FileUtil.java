16b9400ee311b37991e3646d365971797779d6e3
chensj <chensj@letcode.cn>
2016-05-16 11:21:35 +0800

    添加文件处理类

---
+++ src/main/java/cn/letcode/utils/file/FileUtil.java
@@ -0,0 +1,31 @@
+package cn.letcode.utils.file;
+
+import java.io.File;
+
+/**
+ * 文件处理工具类
+ * 
+ * @author chensj
+ *
+ */
+public class FileUtil {
+	/**
+	 * 递归删除文件<br>
+	 * 通过自己调用自己的方式删除文件或者文件夹
+	 * 
+	 * @param file
+	 *            需要删除的文件或者文件夹
+	 */
+	public static void deleteLongFile(File file) {
+		if (file.isDirectory()) {
+			File[] lf = file.listFiles();
+			for (File f : lf)
+				deleteLongFile(f);
+			System.out.println(file.getAbsolutePath());
+			file.delete();
+		} else {
+			System.out.println(file.getAbsolutePath());
+			file.delete();
+		}
+	}
+}
